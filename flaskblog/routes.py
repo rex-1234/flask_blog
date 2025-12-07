b9f6cb6146328aca3417e66ad98b8380a95bde90
rex-1234 <jitendrachoudhary128@gmail.com>
2025-12-07 13:43:20 +0530

    Added CRUD functionality for post

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -1,32 +1,16 @@
 import os
 import secrets
 from PIL import Image
-from flask import render_template, url_for, flash, redirect, request
+from flask import render_template, url_for, flash, redirect, request, abort
 from flask_login import login_user, current_user, logout_user, login_required
-from sqlalchemy import Update
 from flaskblog import app, db, bcrypt
-from flaskblog.forms import RegisterationForm, LoginForm, UpdateAccountForm
+from flaskblog.forms import RegisterationForm, LoginForm, UpdateAccountForm, PostForm
 from flaskblog.models import User, Post
-
-posts = [
-    {
-        'author': 'Elon Musk',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'Nov 30, 2025'
-    },
-    {
-        'author': 'Jeff Bezos',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'Dec 1, 2025'
-    }
-]
-
 
 @app.route("/")
 @app.route("/home")
 def home():
+    posts = db.session.query(Post).order_by(Post.date_posted.desc()).all()
     return render_template("home.html", posts=posts)
 
 @app.route("/about")
@@ -79,7 +63,7 @@ def save_picture(form_picture):
     picture_path = os.path.join(app.root_path, 'static/profile_pics', picture_fname)
 
     # Resize the image before saving
-    output_size = (125, 125)
+    output_size = (200, 200)
     img = Image.open(form_picture)
     img.thumbnail(output_size)
     img.save(picture_path)
@@ -104,3 +88,49 @@ def account():
         form.email.data = current_user.email
     image_file = url_for('static', filename='profile_pics/' + current_user.image_file)
     return render_template('account.html', title='Account', image_file=image_file, form=form)
+
+@app.route("/post/new", methods=['GET', 'POST'])
+@login_required
+def new_post():
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Post(title=form.title.data, content=form.content.data, author=current_user)
+        db.session.add(post)
+        db.session.commit()
+        flash('Your post has been created!', 'success')
+        return redirect(url_for('home'))
+    return render_template('create_post.html', title='New Post', form=form)
+
+@app.route("/post/<post_id>")
+def post(post_id):
+    post = Post.query.get_or_404(post_id)
+    return render_template('post.html', title=post.title, post=post)
+
+@app.route("/post/<post_id>/update", methods=['GET', 'POST'])
+@login_required
+def update_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    form = PostForm()
+    if form.validate_on_submit():
+        post.title = form.title.data
+        post.content = form.content.data
+        db.session.commit()
+        flash('Your post has been updated!', 'success')
+        return redirect(url_for('post', post_id=post.id))
+    elif request.method == 'GET':
+        form.title.data = post.title
+        form.content.data = post.content
+    return render_template('create_post.html', title='Update Post', form=form, post=post)
+
+@app.route("/post/<post_id>/delete", methods=['POST'])
+@login_required
+def delete_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    db.session.delete(post)
+    db.session.commit()
+    flash('Your post has been deleted!', 'success')
+    return redirect(url_for('home'))
